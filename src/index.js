912d8f804eeb6e4126794ade5a9ec9352235aa27
cherukss <cherukss@gmail.com>
2017-10-28 17:53:08 -0400

    uncomment the debugger line to add a breakpoint to index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './index.css';
 import numeral from 'numeral';
 
 const bitcoinValue = numeral(5740.22).format('$1,000.00');
+//debugger;
 console.log(`The value of a bitcoin today is ${bitcoinValue}`);
