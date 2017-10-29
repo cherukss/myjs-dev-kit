bd67436495dd8e624d5599fda9012bb51938d17a
cherukss <cherukss@gmail.com>
2017-10-28 22:08:37 -0400

    configured eslint rules, used eslint-watch with npm scripts to automate linting some config files and src, buildScript folders

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import numeral from 'numeral';
 
 const bitcoinValue = numeral(5740.22).format('$1,000.00');
 //debugger;
-console.log(`The value of a bitcoin today is ${bitcoinValue}`);
+console.log(`The value of a bitcoin today is ${bitcoinValue}`); // eslint-disable-line no-console
